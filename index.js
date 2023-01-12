caaf33f3bea70fafc1f95c26fdb700b49e49894f
Gabriel Sinapsis <gabyreload@gmail.com>
2023-01-12 14:47:16 -0300

    Update default port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`EXPRESS: ${app.get("env")}`);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4444;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // routes
